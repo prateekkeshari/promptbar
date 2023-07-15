fa822c6f1a328c0540863117da3bd9d0d711849f
Prateek K. Keshari <prateekkeshari7@gmail.com>
2023-07-15 17:10:38 +0200

    added persistent storage

--- content.js
+++ content.js
@@ -1,46 +1,115 @@
+// Create an element with an ID
+const createElement = (tagName, id) => {
+    const element = document.createElement(tagName);
+    element.id = id;
+    return element;
+};
+
+// Show or hide an element
+const toggleElement = (element, shouldShow) => {
+    element.style.display = shouldShow ? 'block' : 'none';
+};
+
+// Create an IndexedDB instance
+let db;
+const request = indexedDB.open('promptsDB', 1);
+request.onerror = function(event) {
+    console.log('error opening database');
+};
+request.onsuccess = function(event) {
+    db = event.target.result;
+    loadPrompts();
+};
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    const objectStore = db.createObjectStore('prompts', {keyPath: 'id', autoIncrement: true});
+};
+
+// Function to load prompts from IndexedDB
+function loadPrompts() {
+    const transaction = db.transaction(['prompts'], 'readonly');
+    const objectStore = transaction.objectStore('prompts');
+    objectStore.openCursor().onsuccess = function(event) {
+        const cursor = event.target.result;
+        if (cursor) {
+            // Create the container for the saved prompt
+            let promptContainer = createElement('div', 'promptContainer');
+
+            // Add the text of the saved prompt
+            let promptText = createElement('p', 'promptText');
+            promptText.textContent = cursor.value.value.substring(0, 15) + '...';
+            promptContainer.appendChild(promptText);
+
+            // Add the copy button
+            let copyButton = createElement('button', 'copyButton');
+            copyButton.textContent = 'Copy';
+            copyButton.addEventListener('click', function () {
+                navigator.clipboard.writeText(cursor.value.value).then(() => {
+                    alert('Prompt copied to clipboard!');
+                });
+            });
+            promptContainer.appendChild(copyButton);
+
+            // Add a remove button
+            let removeButton = createElement('img', 'removeButton');
+            removeButton.src = 'remove_icon.svg';
+            removeButton.addEventListener('click', function () {
+                // Show the confirmation dialog
+                toggleElement(confirmDialog, true);
+                yesButton.onclick = function () {
+                    promptContainer.remove();
+                    // Remove the prompt from IndexedDB
+                    const deleteTransaction = db.transaction(['prompts'], 'readwrite');
+                    const deleteObjectStore = deleteTransaction.objectStore('prompts');
+                    const deleteRequest = deleteObjectStore.delete(cursor.value.id);
+                    deleteRequest.onsuccess = function(event) {
+                        console.log('Prompt removed from IndexedDB');
+                    };
+                    toggleElement(confirmDialog, false);
+                };
+            });
+            promptContainer.appendChild(removeButton);
+
+            sidebar.appendChild(promptContainer);
+
+            cursor.continue();
+        }
+    };
+}
+
 // Create the sidebar
-let sidebar = document.createElement('div');
-sidebar.id = 'sidebar';
+const sidebar = createElement('div', 'sidebar');
 document.body.appendChild(sidebar);
 
 // Create the show button
-let showButton = document.createElement('img');
-showButton.id = 'showButton';
+const showButton = createElement('img', 'showButton');
 showButton.src = 'menu_icon.svg';
-showButton.title = "Show sidebar"; // Added title for better accessibility
-showButton.addEventListener('mouseover', function () { showButton.style.opacity = '0.7'; }); // Added hover state
-showButton.addEventListener('mouseout', function () { showButton.style.opacity = '1'; }); // Reset on mouse out
-showButton.addEventListener('click', function () {
-    sidebar.style.display = 'block';
-    showButton.style.display = 'none';
+showButton.alt = "Show sidebar";
+showButton.addEventListener('click', () => {
+    toggleElement(sidebar, true);
+    toggleElement(showButton, false);
     document.body.style.marginRight = '20%';
 });
 document.body.appendChild(showButton);
 
 // Create the hide button
-let hideButton = document.createElement('img');
-hideButton.id = 'hideButton';
+const hideButton = createElement('img', 'hideButton');
 hideButton.src = 'close_icon.svg';
-hideButton.title = "Hide sidebar"; // Added title for better accessibility
-hideButton.addEventListener('mouseover', function () { hideButton.style.opacity = '0.7'; }); // Added hover state
-hideButton.addEventListener('mouseout', function () { hideButton.style.opacity = '1'; }); // Reset on mouse out
-hideButton.addEventListener('click', function () {
-    sidebar.style.display = 'none';
-    showButton.style.display = 'block';
+hideButton.alt = "Hide sidebar";
+hideButton.addEventListener('click', () => {
+    toggleElement(sidebar, false);
+    toggleElement(showButton, true);
     document.body.style.marginRight = '0';
 });
 sidebar.appendChild(hideButton);
 
-
 // Create the textarea for entering a new prompt
-let newPromptInput = document.createElement('textarea');
-newPromptInput.id = 'newPromptInput';
+const newPromptInput = createElement('textarea', 'newPromptInput');
 newPromptInput.placeholder = 'Enter a new prompt...';
 sidebar.appendChild(newPromptInput);
 
 // Create the hint for saving a prompt
-let saveHint = document.createElement('p');
-saveHint.id = 'saveHint';
+const saveHint = createElement('p', 'saveHint');
 saveHint.textContent = 'Hit ↩️ to save';
 sidebar.appendChild(saveHint);
 
@@ -50,43 +119,39 @@ newPromptInput.addEventListener('keypress', function (e) {
         let newPrompt = newPromptInput.value;
         if (newPrompt.trim() !== '') {
             // Create the container for the saved prompt
-            let promptContainer = document.createElement('div');
-            promptContainer.id = 'promptContainer';
+            let promptContainer = createElement('div', 'promptContainer');
 
             // Add the text of the saved prompt
-            let promptText = document.createElement('p');
-            promptText.id = 'promptText';
+            let promptText = createElement('p', 'promptText');
             promptText.textContent = newPrompt.substring(0, 15) + '...';
             promptContainer.appendChild(promptText);
 
             // Add the copy button
-            let copyButton = document.createElement('button');
-            copyButton.id = 'copyButton';
+            let copyButton = createElement('button', 'copyButton');
             copyButton.textContent = 'Copy';
             copyButton.addEventListener('click', function () {
-                let tempInput = document.createElement('input');
-                document.body.appendChild(tempInput);
-                tempInput.value = newPrompt;
-                tempInput.select();
-                document.execCommand('copy');
-                document.body.removeChild(tempInput);
-                alert('Prompt copied to clipboard!');
+                navigator.clipboard.writeText(newPrompt).then(() => {
+                    alert('Prompt copied to clipboard!');
+                });
             });
             promptContainer.appendChild(copyButton);
 
             // Add a remove button
-            let removeButton = document.createElement('img');
-            removeButton.id = 'removeButton';
+            let removeButton = createElement('img', 'removeButton');
             removeButton.src = 'remove_icon.svg';
             removeButton.addEventListener('click', function () {
                 // Show the confirmation dialog
-                confirmDialog.style.display = 'block';
+                toggleElement(confirmDialog, true);
                 yesButton.onclick = function () {
                     promptContainer.remove();
-                    // Remove the prompt from Chrome storage
-                    chrome.storage.local.remove(newPrompt);
-                    // Hide the confirmation dialog
-                    confirmDialog.style.display = 'none';
+                    // Remove the prompt from IndexedDB
+                    const transaction = db.transaction(['prompts'], 'readwrite');
+                    const objectStore = transaction.objectStore('prompts');
+                    const request = objectStore.delete(newPrompt);
+                    request.onsuccess = function(event) {
+                        console.log('Prompt removed from IndexedDB');
+                    };
+                    toggleElement(confirmDialog, false);
                 };
             });
             promptContainer.appendChild(removeButton);
@@ -94,30 +159,32 @@ newPromptInput.addEventListener('keypress', function (e) {
             sidebar.appendChild(promptContainer);
             newPromptInput.value = '';
 
-            // Save the prompt to Chrome storage
-            chrome.storage.local.set({ [newPrompt]: true });
+            // Save the prompt to IndexedDB
+            const transaction = db.transaction(['prompts'], 'readwrite');
+            const objectStore = transaction.objectStore('prompts');
+            const request = objectStore.add({id: newPrompt, value: newPrompt});
+            request.onsuccess = function(event) {
+                console.log('Prompt added to IndexedDB');
+            };
         }
     }
 });
 
 // Create the confirmation dialog
-let confirmDialog = document.createElement('div');
-confirmDialog.id = 'confirmDialog';
+const confirmDialog = createElement('div', 'confirmDialog');
 confirmDialog.textContent = 'Are you sure you want to delete this prompt?';
 sidebar.appendChild(confirmDialog);
 
 // Create the yes button for the confirmation dialog
-let yesButton = document.createElement('button');
-yesButton.id = 'yesButton';
+const yesButton = createElement('button', 'yesButton');
 yesButton.textContent = 'Yes';
 confirmDialog.appendChild(yesButton);
 
 // Create the no button for the confirmation dialog
-let noButton = document.createElement('button');
-noButton.id = 'noButton';
+const noButton = createElement('button', 'noButton');
 noButton.textContent = 'No';
-noButton.addEventListener('click', function () {
+noButton.addEventListener('click', () => {
     // Hide the confirmation dialog
-    confirmDialog.style.display = 'none';
+    toggleElement(confirmDialog, false);
 });
 confirmDialog.appendChild(noButton);
